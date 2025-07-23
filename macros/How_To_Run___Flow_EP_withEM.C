5d585c250b180ee4d368ef897375b43e3fceb1e9
thakurd3 <thakurdhana@gmail.com>
2025-07-23 17:15:12 +0200

    Create How_To_Run___Flow_EP_withEM.C

---
+++ macros/How_To_Run___Flow_EP_withEM.C
@@ -0,0 +1,4 @@
+How to Run: 
+1: Put FlowAnalysis_Fitting.h,cxx and FlowAnalysis_Helper.h,cxx in the same directory
+1: Compile -->.L Flow_EP_withEM.C++g
+2: Run ----> Flow_EP_withEM(10,0,4,1,0,0)  //PtBins, CB2(data), EventMixing, flag_2, run2, run2yield (first 4 are important), for more detail look into the macro
